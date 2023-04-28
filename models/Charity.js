7ba30b551df8ba35c6b82eddc2b0eb8e4b598dce
Omarwhite30 <omarwhite2@aol.com>
2023-04-28 15:39:51 -0400

    created charity model

---
+++ models/Charity.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+let Charity = new Schema({
+  name: { type: String },
+  category: { type: String },
+  private_donations: { type: String },
+  total_revenue: { type: String },
+  fundraising_efficiency: { type: Number },
+  charitable_commitment: { type: Number },
+  
+});
+
+export default mongoose.model("Charity", Charity);

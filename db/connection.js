01bad7ac62795a66112d1a174b96e6f2935003c0
demetrigeras <demetrijgeras@gmail.com>
2023-04-28 15:47:01 -0400

    adjust to json file name and location

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import chalk from "chalk";
+
 
 const MONGODB_URI =
-  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/";
+  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/world-help";
 
 // This is for Model.findByIdAndUpdate method, specifically so that {new: true} is the default
 mongoose.set("returnOriginal", false);
@@ -14,11 +14,11 @@ mongoose
   );
 
 mongoose.connection.on("disconnected", () =>
-  console.log(chalk.bold("Disconnected from MongoDB!"))
+  console.log("Disconnected from MongoDB!")
 );
 
 mongoose.connection.on("error", (error) =>
-  console.error(chalk.red(`MongoDB connection error: ${error}`))
+  console.error(`MongoDB connection error: ${error}`)
 );
 
 export default mongoose.connection;

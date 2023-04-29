aff4c6bf55569704ffa0b93c06b36af7f6b06010
demetrigeras <demetrijgeras@gmail.com>
2023-04-29 13:59:48 -0400

    added cors in the index.js but commented out logger

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import db from "./db/connection.js";
 import routes from "./routes/index.js";
-
 import express from "express";
+import cors from 'cors'
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
-app.use(logger("dev"));
+// app.use(logger("dev"));
+app.use(cors("dev"))
 
 app.use("/world-help", routes);
 
 db.on("connected", () => {
   console.clear();
-  console.log(chalk.blue("Connected to MongoDB!"));
+  console.log("Connected to MongoDB!");
 
   app.listen(PORT, () => {
     process.env.NODE_ENV === "production"

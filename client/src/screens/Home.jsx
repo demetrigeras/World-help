a88dbbe48f6c17339159a5e46f5581a0d693ef8e
hoyoungsin <hoyoungsin@gmail.com>
2023-04-30 13:36:23 -0400

    installed react-router-dom and created link and routers. made directories for components and screens. Nav component created. Home screen started(header made).

---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div className="header">
+            <h1>Welcome to World Help!</h1>
+            <p>Click on any chairitable organization below to get more information!</p>
+        </div>
+    </div>
+  )
+}

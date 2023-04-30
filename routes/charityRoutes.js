251010cddd8af2bdffdaf59949ff28741d39a2e5
demetrigeras <demetrijgeras@gmail.com>
2023-04-30 15:30:28 -0400

    index.js

--- routes/charityRoutes.js
+++ routes/charityRoutes.js
@@ -1,12 +1,1 @@
-import { Router } from "express";
-import * as controllers from "../controllers/charitycontroller.js"
-
-const router = Router();
-
-router.get("/", controllers.getCharities);
-router.get("/:id", controllers.getCharity);
-router.post("/", controllers.createCharity);
-router.put("/:id", controllers.updateCharity);
-router.delete("/:id", controllers.deleteCharity);
-
-export default router;
+r

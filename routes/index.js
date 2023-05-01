15329bced13ce673e68877fa336d6474e8eeb34f
demetrigeras <demetrijgeras@gmail.com>
2023-05-01 14:28:25 -0400

    backend sign in and sign up imported

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
 import charityRoutes from './charityRoutes.js'
 import donorRoutes from './donorRoutes.js'
+import usersRoutes from './users.js'
 
 const router = Router();
 
@@ -8,5 +9,6 @@ router.get("/", (req, res) => res.send("This is the charity api root!"));
 
 router.use("/charity", charityRoutes);
 router.use("/donor", donorRoutes)
+router.use('/', usersRoutes)
 
 export default router;

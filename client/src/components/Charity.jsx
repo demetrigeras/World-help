8ff9c430c1692f7e4aab6b03bda653348055cfe4
Pearse O’Donohue <pearseodonohue@Pearses-MacBook-Air-2.local>
2023-05-01 16:09:00 -0400

    deployement and some css

--- client/src/components/Charity.jsx
+++ client/src/components/Charity.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Charity({charity}) {
     return (
         <div>
-            <Link to={`/charity/${charity._id}`} >
-                <img className="logo" src={charity.logo} alt={charity.name} />
+            <Link className="logo" to={`/charity/${charity._id}`} >
+                <img  src={charity.logo} alt={charity.name} />
             </Link>
         </div>
     )

fda7c3023ac5a284622e8d2f7f879d9b663d42f6
demetrigeras <demetrijgeras@gmail.com>
2023-05-03 15:31:12 -0400

    possible netlify fix

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import Charity from "../components/Charity.jsx";
 import { getCharities } from "../services/charities.js";
 

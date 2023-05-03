feb8cd9051f228ebcf52fa28c9d961e73a903df1
Pearse O’Donohue <pearseodonohue@Pearses-MacBook-Air-2.local>
2023-05-03 10:00:10 -0400

    Home/Charity Screen CSS almost done

--- client/src/screens/Charity.jsx
+++ client/src/screens/Charity.jsx
@@ -38,32 +38,45 @@ export default function Charity(props) {
   }
 
   return (
-    <div>
-      <h1>{charity.name}</h1>
-      <p>{charity.mission_statement}</p>
-      <div className='charityInfo'>
-        <p>Mission Statement: {charity.mission_statements}</p>
-        <p>Cause Category: {charity.category}</p>
-        <p>Private Donations: {charity.private_donations}</p>
-        <p>Total Revenue: {charity.total_revenue}</p>
-        <p>Fundrising Efficiency: {charity.fundraising_efficiency}%</p>
-        <p>Charitable Commitments: {charity.charitable_commitment}%</p>
-        <p>Click the link below to donate!</p>
-        <a href={charity.website}>{charity.website}</a>
+  <>  
+    <div className="charity-container">
+
+      <div className="top-container">
+        <h1>{charity.name}</h1>
+        <p>{charity.mission_statement}</p>
       </div>
-        <div className='Pledges'>
-        <header>Pledges</header>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input type="text" placeholder="Enter your Amount" onChange={handleChange}/>
-          <input type="submit" value="Pledge!" />
-        </form>
-        
-        <div>World-Help User Pledges</div>
-        <div>Pledge Placeholder</div>
 
+      <div className="left-container">
+        <div className='charityInfo'>
+          <p>Mission Statement: {charity.mission_statements}</p>
+          <p>Cause Category: {charity.category}</p>
+          <p>Private Donations: {charity.private_donations}</p>
+          <p>Total Revenue: {charity.total_revenue}</p>
+          <p>Fundrising Efficiency: {charity.fundraising_efficiency}%</p>
+          <p>Charitable Commitments: {charity.charitable_commitment}%</p>
+          <p>Click the link below to donate!</p>
+          <a href={charity.website}>{charity.website}</a>
         </div>
+      </div>
 
+      <div className="right-container">
+
+          <div className='Pledges'>
+            <header>Pledges</header>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input type="text" placeholder="Enter your Amount" onChange={handleChange}/>
+              <input type="submit" value="Pledge!" />
+            </form>
+          </div>
+
+          <div className="bottom-right-container">
+            <div>World-Help User Pledges</div>
+            <div>Pledge Placeholder</div>
+          </div>
+          
+      </div>
 
     </div>
+    </>
   )
 }

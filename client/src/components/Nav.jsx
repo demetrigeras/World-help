4f14cba4adad71796c0a06135c6cbb7b00ba717f
Omarwhite30 <omarwhite2@aol.com>
2023-05-03 10:01:04 -0400

    updated

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,16 +5,19 @@ export default function Nav(props) {
   console.log({user}, user)
 
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      
+    <Nav>
+      <div className='home'>    
+          <NavLink to="/">Home</NavLink>
+      </div>
+      <div className='sign-in/out'>
       {user ?
       <NavLink to="/sign-out">Sign Out</NavLink>:(
       <>
       <NavLink to="/sign-up">Sign Up</NavLink>
       <NavLink to="/sign-in">Sign In</NavLink>
       </>)}
-    </nav>
+      </div>
+    </Nav>
 
   )
 }
